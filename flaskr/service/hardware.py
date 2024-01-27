12c5925141ad9049a4f637d03c16b230976e1a73
Rainhard Kriegler <rainhard.kriegler@gmail.com>
2024-01-27 09:45:11 +0100

    added commandline read of cpu temperature and build image for execution on raspberry

--- flaskr/service/hardware.py
+++ flaskr/service/hardware.py
@@ -1,7 +1,10 @@
 import random
+import os
 
 
 class Hardware:
 
     def get_cpu_temp(self):
-        return random.uniform(30.0, 40.0)
+        #return random.uniform(30.0, 40.0)
+        temperatur=os.popen("vcgencmd measure_temp").read()
+        return temperatur[5:(len(temperatur)-2)]
