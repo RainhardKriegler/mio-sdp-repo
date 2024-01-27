1041a5ab6dc4714365024028a9720dcc1f489b24
Rainhard Kriegler <rainhard.kriegler@gmail.com>
2024-01-27 14:03:44 +0100

    switched from deprecated os to subprocess to read the temperature

--- flaskr/service/hardware.py
+++ flaskr/service/hardware.py
@@ -1,8 +1,8 @@
-import os
+import subprocess
 
 
 class Hardware:
 
     def get_cpu_temp(self):
-        temperatur = os.popen("vcgencmd measure_temp").read()
+        temperatur = subprocess.run("vcgencmd measure_temp", shell=True, stdout=subprocess.PIPE, text=True)
         return temperatur[5:(len(temperatur)-2)]
