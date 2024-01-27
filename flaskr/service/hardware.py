cea0dd3a3d9dce386f86a2301b71958b6e27cb26
Rainhard Kriegler <rainhard.kriegler@gmail.com>
2024-01-27 14:10:29 +0100

    added linebreak to fix lint issue

--- flaskr/service/hardware.py
+++ flaskr/service/hardware.py
@@ -4,5 +4,6 @@ import subprocess
 class Hardware:
 
     def get_cpu_temp(self):
-        temperatur = subprocess.run("vcgencmd measure_temp", shell=True, stdout=subprocess.PIPE, text=True)
+        temperatur = subprocess.run("vcgencmd measure_temp",
+                     shell=True, stdout=subprocess.PIPE, text=True)
         return temperatur[5:(len(temperatur)-2)]
